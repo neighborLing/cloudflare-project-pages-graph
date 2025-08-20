e2b79ec1ec9686e67fd317cac367d4aa4cb86bae
lingtong <lingt1@landray.com.cn>
2025-08-20 18:38:20 +0800

    feat: 更新线上请求

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,7 +1,21 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+const getGraphQLUri = () => {
+  if (typeof window === 'undefined') {
+    return 'http://localhost:8787/graphql';
+  }
+  
+  const isLocalhost = window.location.hostname === 'localhost' || 
+                     window.location.hostname === '127.0.0.1' ||
+                     window.location.hostname === '';
+  
+  return isLocalhost 
+    ? 'http://localhost:8787/graphql'
+    : 'https://meadery.win/api/graphql';
+};
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8787/graphql',
+  uri: getGraphQLUri(),
 });
 
 export const apolloClient = new ApolloClient({
